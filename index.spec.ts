577820b58aa5cb1621412b5b0f308dfb4ac080a2
Arnaud Buchholz <arnaud.buchholz@free.fr>
2024-11-11 11:06:47 -0500

    feat: assess parallel execution count

--- index.spec.ts
+++ index.spec.ts
@@ -18,3 +18,20 @@ it('runs the processor on all items of the queue', async () => {
   expect(results.every(result => result.status === 'fulfilled'));
   expect(getFulfilledValues(results)).toStrictEqual([1, 2, 3, 4, 5, 6]);
 });
+
+const sleep = (ms: number) => new Promise(resolve => setTimeout(resolve, ms));
+
+it('executes everything in parallel', async () => {
+  const INPUT_SIZE = 100;
+  let parallelCount = 0;
+  let count = 0;
+  const results = await parallelize(async (i: number) => {
+    ++count;
+    parallelCount = Math.max(parallelCount, count);
+    await sleep(10);
+    --count;
+    return i +1;
+  }, new Array(INPUT_SIZE).fill(0));
+  expect(getFulfilledValues(results).every(value => value === 1)).toStrictEqual(true);
+  expect(parallelCount).toStrictEqual(INPUT_SIZE);
+});
